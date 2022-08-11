6f814e9794f7ff3e4f8c81bd5516fc93a5dbf470
Tauseef-Iqbal <95753230+Tauseef-Iqbal@users.noreply.github.com>
2022-08-11 15:14:35 +0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/hello', function (req, res, next) {
-  res.send('Running Perfectly');
+  res.send('Running Perfectly pull');
 });
 
 module.exports = router;
